113dfb7578c15fe327336739345f65095f59c102
Cody Eddie <buffbuck0427@gmail.com>
2022-08-21 22:02:34 -0600

    stuff on projects pages

--- src/components/Projects /index.js
+++ src/components/Projects /index.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import Jacket from '../../assets/imgs/weather-thoughts.jpg'
+import Scrunchies from '../../assets/imgs/mirandas-scrunchies-dashboard.jpg'
+import Watch from '../../assets/imgs/what-to-watch.png'
 
 function Projects() {
   const project1 = <a href="https://attidack.github.io/weather-thoughts/"> Do You Need A Jacket?</a>
@@ -13,23 +16,20 @@ function Projects() {
     <section>
       <h1 className='proHeader'>Projects:</h1>
 
-      <div id='portfolio'>
-        <div>
+      <div id='projects'>
+        <div className='card'>
           <h3>{project1}</h3>
-          {/* <img src={Quiz} alt='Quiz screenshot'></img> */}
-          {/* <h5>{QuizRepo}</h5> */}
-        </div>
+          <img src={Jacket} alt=''></img>
+        </div> 
 
-        <div>
+        <div className='card'>
           <h3>{project2}</h3>
-          {/* <img src={Schedule} alt=''></img>
-      <h5>{ScheduleRepo}</h5> */}
+          <img src={Scrunchies} alt=''></img>
         </div>
 
-        <div>
+        <div className='card'>
           <h3>{project3}</h3>
-          {/* <img src={Schedule} alt=''></img>
-      <h5>{ScheduleRepo}</h5> */}
+          <img src={Watch} alt=''></img>
         </div>
       </div>
     </section>
